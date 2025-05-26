0bb5a71b3119ca5f318fddf32f1c752f78473bf1
george10hany77 <georgehany064@gmail.com>
2025-05-26 13:58:25 +0000

    adding csv_racingline_publisher node

--- launch/csv_racingline_publisher.launch.py
+++ launch/csv_racingline_publisher.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     ld = LaunchDescription()
     
     param_path = os.path.join(get_package_share_directory("trajectory_planning"), "config", "csv_pub_config.yaml")
-    csv_path = Node(
+    csv_path_pub = Node(
             package='trajectory_planning',
             executable='csv_pub_exe',
             name='csv_path_publisher_node',
@@ -15,6 +15,6 @@ def generate_launch_description():
             output='screen'
         )
     
-    ld.add_action(csv_path)
+    ld.add_action(csv_path_pub)
 
     return ld
